1cd1b6f5989ccb314e3cee364fab15947ea329ac
Albert Pratomo <albertpratomo21@gmail.com>
2023-11-22 00:58:35 +0100

    feat: remove unnecessary components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
-  <main class="px-10 py-20 text-center">
+  <main class="p-10">
     <slot />
+
     <Footer />
-    <div class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
-    </div>
   </main>
 </template>
